277c84f5cd3318ba500a905277c595527fb2967e
yutai <1583286821@qq.com>
2021-05-05 14:40:27 +0800

    增加echarts

--- src/main.js
+++ src/main.js
@@ -9,6 +9,29 @@ import CollapseTransition from 'element-ui/lib/transitions/collapse-transition' 
 
 import './mock/index' /* 路由拦截 */
 
+// 引入 echarts 核心模块，核心模块提供了 echarts 使用必须要的接口。
+import * as echarts from 'echarts/core';
+// 引入折线图图表，图表后缀都为 Chart
+import {
+  LineChart
+} from 'echarts/charts';
+// 引入提示框，标题，直角坐标系组件，组件后缀都为 Component
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  LegendComponent
+} from 'echarts/components';
+// 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
+import {
+  CanvasRenderer
+} from 'echarts/renderers';
+
+// 注册必须的组件
+echarts.use(
+  [TitleComponent, TooltipComponent, GridComponent, LineChart, CanvasRenderer, LegendComponent]
+);
+
 import {
   Container,
   Header,
@@ -57,6 +80,8 @@ Vue.use(FormItem)
 Vue.use(Input)
 
 Vue.component(CollapseTransition.name, CollapseTransition) /* element-ui 动画 */
+
+Vue.prototype.$Echarts = echarts
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,101 @@
 <template>
-  <div>
-    <div id="mount-point">home
-      <el-button v-permission="{action: 'add', effect: 'disabled'}" @click="addHandle" type="primary">添加按钮</el-button>
-      <el-button type="warning">修改按钮</el-button>
-      <el-button type="danger">删除按钮</el-button>
-    </div>
-    <div v-for="item in 50" :key="item">{{item}}修改后</div>
+  <div id="main" style="width: 100%; height: 600px">
+
   </div>
 </template>
 
 <script>
-import { add } from '../api/Home/Home'
-export default {
-  name: 'Home',
-  components: {
-  },
-  data () {
-    return {
 
-    }
-  },
-
-  mounted () {
-
-  },
-
-  methods: {
-    // 点击添加的操作
-    async addHandle () {
-      const data = await add()
-      if (data) {
-        console.log(data)
+  export default {
+    name: 'Home',
+    components: {},
+    data() {
+      return {
+        myChart: null,
+        seriesData: [820, 932, 901, 934, 1290, 1330, 1320, 1786]
       }
-    }
-  }
+    },
 
-}
+    watch: {
+      /* 监听数组变化 进行重绘 */
+      seriesData(){
+        this.setOptions()
+      }
+    },
+
+    mounted() {
+
+      // 监听页面变化 重置图表尺寸
+      addEventListener('resize', this.resizeInnerWidth)
+      // 接下来的使用就跟之前一样，初始化图表，设置配置项
+      this.myChart = this.$Echarts.init(document.getElementById('main'));
+      this.setOptions()
+    },
+
+    methods: {
+      setOptions(){
+        this.myChart.setOption({
+          title: {
+            show: true,
+            text: '一周访问量',
+            left: 'center'
+          },
+          grid: {left: '7%', top: '7%', right: '3%', bottom: '7%'},
+          xAxis: {
+            type: 'category',
+            splitLine: {
+              show: true
+            },
+            data: ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'],
+            splitArea:{
+              show: true,
+              areaStyle: {
+                color: ['rgba(250,250,250,0.3)','rgba(233,238,243,0.3)']
+              }
+            }
+          },
+          yAxis: {
+            type: 'value',
+            splitArea:{
+              show: true,
+              areaStyle: {
+                color: ['rgba(250,250,250,0.3)','rgba(233,238,243,0.3)']
+              }
+            }
+          },
+          tooltip: {
+            show: true,
+            trigger: 'axis', // 提示框触发方式
+            axisPointer:{
+              label:{
+                show: true
+              },
+            },
+            backgroundColor: 'rgba(50,50,50,0.7)', // 提示框背景
+            borderColor: '#ccc', // 提示窗边框颜色
+            textStyle: { // 提示窗文字颜色
+              color: '#fff'
+            },
+            formatter: '浏览人数: {c0}人' // 提示框内容模板
+          },
+          series: [{ // 折线图
+            data: this.seriesData,
+            type: 'line',
+            smooth: true
+          }]
+        })
+      },
+      resizeInnerWidth(){
+        this.myChart.resize()
+      }
+    },
+
+    beforeDestroy() {
+      /* 组件销毁之前 卸载监听事件 */
+      removeEventListener('resize', this.this.resizeInnerWidth)
+    }
+
+  }
 </script>
 
 <style lang="scss">
